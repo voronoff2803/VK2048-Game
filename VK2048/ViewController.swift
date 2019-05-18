e5206d00cad70f7d3101106e7fde85866d4ef4f8
Alexey Voronov <voronoff2803@gmail.com>
2019-05-18 21:14:09 +0300

    Initial Commit

---
+++ VK2048/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK2048
+//
+//  Created by Alexey Voronov on 18/05/2019.
+//  Copyright © 2019 Alexey Voronov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
